2db319b499abfddce69393c012972892fe873262
Supriya11 <101583227+supriya5590@users.noreply.github.com>
2022-05-05 16:10:58 +0530

    Add files via upload

---
+++ twodimensionalarray.java
@@ -0,0 +1,31 @@
+package Day12;
+//wap to design 3*4 matrix
+public class twodimensionalarray {
+
+	public static void main(String[] args) {
+
+    int r[][]= new int[3][4];
+    int y[][];
+    
+    y= new int[3][4];
+    
+    
+    int y2[][]= {{1,2},   {3,5}};
+    
+    
+    
+    String j[][];
+    j[0][0]="neha";
+    j[0][1]="ashu";
+    j[1][0]="ajay";
+    j[1][1]="pooja";
+    
+    
+    
+    		
+    
+    }
+
+	}
+
+}
